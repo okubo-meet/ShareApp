ffd7b906ac53b97db72750df0abb454b0b31c0f1
大久保徹郎 <okubo@office-meet.com>
2023-02-15 16:28:54 +0900

    プロジェクト作成

---
+++ ShareApp-okubo/ShareApp-okubo/ShareApp_okuboApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareApp_okuboApp.swift
+//  ShareApp-okubo
+//
+//  Created by 大久保徹郎 on 2023/02/15.
+//
+
+import SwiftUI
+
+@main
+struct ShareApp_okuboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
